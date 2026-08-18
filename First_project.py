482ed656aa8e45452300bf72d152f540dd462b91
EagleG66 <glenn.gudmunson@ucas-edu.net>
2026-08-18 09:36:14 -0600

    larose

--- First_project.py
+++ First_project.py
@@ -2,28 +2,48 @@
 import math
 import time
 
-#a = int(input("What is A? "))
-#b = int(input("What is B? "))
-#c = int(input("What is C? "))
 
-#answer1 = -b + math.sqrt(b^2 -4*c*a)/2*a
-#answer2 = -b - math.sqrt(b^2 -4*c*a)/2*a
+#MATH
+"""
+a = int(input("What is A? "))
+b = int(input("What is B? "))
+c = int(input("What is C? "))
 
-#print("The answers are " + answer1 + " and " + answer2)
+answer1 = -b + math.sqrt(b^2 -4*c*a)/2*a
+answer2 = -b - math.sqrt(b^2 -4*c*a)/2*a
+str1 = str(answer1)
+str2 = str(answer2)
 
-x = 0
+print("The answers are " + str1 + " and " + str2)
+
+"""
+#TIMER
+
+x = 1
 y = 0
 running = True
 
-while running:
-    print(y + ":" + x)
 
-    if x >= 60:
+while running:
+
+    strY = str(y)
+    strX = str(x)
+
+    if x >= 10:
+        print(strY + ":" + strX)
+    else:
+        print(strY + ":0" + strX)
+
+    if x >= 59:
         x = 0
         y +=1
 
+    if y >= 59:
+        x = 0
+        y = 0
 
-    x+=1
+
+    x += 1
     time.sleep(1)
 
 
