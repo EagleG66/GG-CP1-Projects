aa221890e1e6beef0072ad56f26b0704e43c364b
EagleG66 <glenn.gudmunson@ucas-edu.net>
2026-08-20 09:37:20 -0600

    I made character intro

---
+++ character_intro.py
@@ -0,0 +1,9 @@
+# Glenn Gudmunson - character intro
+
+character_name = "Kaidon Sparks"
+character_age = 17
+character_school = "ASU"
+character_profession = "Student"
+
+
+print("Hi, my name is",character_name,"and I am a",character_age,"year old",character_profession,"at",character_school,".")
