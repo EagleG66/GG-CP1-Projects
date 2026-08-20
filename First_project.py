81684bda5387c663fd4d22c57552663550ee7cdc
EagleG66 <glenn.gudmunson@ucas-edu.net>
2026-08-20 09:05:52 -0600

    made math homework and variable notes

--- First_project.py
+++ First_project.py
@@ -1,27 +1,14 @@
 #Glenn Gudmunson FIRST PROJECT
-import math
 import time
 
-
-#MATH
-"""
-a = int(input("What is A? "))
-b = int(input("What is B? "))
-c = int(input("What is C? "))
-
-answer1 = -b + math.sqrt(b^2 -4*c*a)/2*a
-answer2 = -b - math.sqrt(b^2 -4*c*a)/2*a
-str1 = str(answer1)
-str2 = str(answer2)
-
-print("The answers are " + str1 + " and " + str2)
-
-"""
 #TIMER
-
-x = 1
-y = 0
+"""
+startY = int(input("How many minutes do you want to set the timer for?"))
+startX = int(input("How many seconds do you want to set the timer for?"))
+x = startX
+y = startY
 running = True
+alarm = False
 
 
 while running:
@@ -29,22 +16,40 @@ while running:
     strY = str(y)
     strX = str(x)
 
-    if x >= 10:
-        print(strY + ":" + strX)
+    if alarm :
+        print("BEEP!")
     else:
-        print(strY + ":0" + strX)
+        if x >= 10:
+            print(strY + ":" + strX)
+        else:
+            print(strY + ":0" + strX)
 
-    if x <= 0:
-        x = 59
+    if x <= -1 and alarm == False:
+        x = 60
         y -=1
 
-    if y <= 0:
-        x = 0
-        y = 0
+    if y <= 0 and x <= 0:
+        alarm = True
 
-
-    x += 1
+    if alarm:
+        print("BEEP!")
+    else:
+        x -= 1
     time.sleep(1)
+"""
+
+
+#math homework
+def multiply(num1,num2):
+    global answer
+    correct_answer = num1 * num2
+    answer = input("What is" + num1 + "x" + num2)
+    if answer == correct_answer:
+        print("You are correct!")
+    else:
+        print("You are wrong!")
+
+multiply(2,2)
 
 
     
---
+++ Variables.py
@@ -0,0 +1,8 @@
+# variable notes
+
+snake_case = "Type of var name, used for variables"
+
+camelCase = "Type of var name, used for functions"
+
+PascalCase = "Type of var name, used for classes"
+
