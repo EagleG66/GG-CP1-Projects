8c5c9710b1d7392cb1b9a45fcc74ffafaa29f1b8
EagleG66 <glenn.gudmunson@ucas-edu.net>
2026-08-24 09:13:57 -0600

    I added the comments to debugging

--- debugging.py
+++ debugging.py
@@ -1,4 +1,6 @@
+# GLENN GUDMUNSON debugging
+
 name = "Alex"
 age = 16
-
+# Name did not have an "e" so I added it, and the print was missing the last parentheses so I added it.
 print("My name is " + name + " and I am " + str(age) + " years old.")
