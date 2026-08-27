c4cbe0f966e004ff0473239a4116b34f8c398b99
EagleG66 <glenn.gudmunson@ucas-edu.net>
2026-08-27 09:52:06 -0600

    TIC TAC TOE IS BEUTIFUL!!!!

--- Im_bored.py
+++ Im_bored.py
@@ -138,18 +138,20 @@ while True:
 
 # TIC TAC TOE
 
-top_left = None
-top_mid = None
-top_right = None
-mid_right = None
-mid_left = None
-mid_mid = None
-bottom_left = None
-bottom_right = None
-bottom_mid = None
+top_left = " "
+top_mid = " "
+top_right = " "
+mid_right = " "
+mid_left = " "
+mid_mid = " "
+bottom_left = " "
+bottom_right = " "
+bottom_mid = " "
+
+game_running = True
 
 
-
+# GAMEBOARD
 print("       |       |       ")
 print("   1   |   2   |   3   ")
 print("       |       |       ")
@@ -162,33 +164,97 @@ print("       |       |       ")
 print("   7   |   8   |   9   ")
 print("       |       |       ")
 
-while True:
-    try:
-        player1_turn = int(input("Choose a spot based on the correlating number from the diagram above"))
-    except:
-        print("THAT'S NOT A NUMBER!!!")
-    else:
-        if player1_turn < 0 or player1_turn > 9:
-            print("THAT'S NOT AN OPTION!!!")
+
+
+def gameBoard():
+    print("       |       |       ")
+    print("   "+top_left+"   |   "+top_mid+"   |   "+top_right+"   ")
+    print("       |       |       ")
+    print("-------+-------+-------")
+    print("       |       |       ")
+    print("   "+mid_left+"   |   "+mid_mid+"   |   "+mid_right+"   ")
+    print("       |       |       ")
+    print("-------+-------+-------")
+    print("       |       |       ")
+    print("   "+bottom_left+"   |   "+bottom_mid+"   |   "+bottom_right+"   ")
+    print("       |       |       ")
+
+
+
+while game_running:
+
+    # PLAYER ONE INPUT
+    while True:
+        try:
+            player1_turn = int(input("Choose a spot based on the correlating number from the diagram above: "))
+        except:
+            print("THAT'S NOT A NUMBER!!!")
         else:
-            break
+            if player1_turn < 0 or player1_turn > 9:
+                print("THAT'S NOT AN OPTION!!!")
+            else:
+                break
+
+    
+    # PLAYER ONE OUTPUT
+    if player1_turn == 1 and top_left == " ":
+        top_left = "X"
+    elif player1_turn == 2 and top_mid == " ":
+        top_mid = "X"
+    elif player1_turn == 3 and top_right == " ":
+        top_right = "X"
+    elif player1_turn == 4 and mid_left == " ":
+        mid_left = "X"
+    elif player1_turn == 5 and mid_mid == " ":
+        mid_mid = "X"
+    elif player1_turn == 6 and mid_right == " ":
+        mid_right = "X"
+    elif player1_turn == 7 and bottom_left == " ":
+        bottom_left = "X"
+    elif player1_turn == 8 and bottom_mid == " ":
+        bottom_mid = "X"
+    elif player1_turn == 9 and bottom_right == " ":
+        bottom_right = "X"
+    else:
+        print("YOU FORFEIT YOUR TURN FOR CHOOSING A SPACE THAT WAS TAKEN UP!")
+
+    gameBoard()
 
 
-if player1_turn == 1 and top_left == None:
-    top_left = "X"
-if player1_turn == 2 and top_mid == None:
-    top_mid = "X"
-if player1_turn == 3 and top_right == None:
-    top_right = "X"
-if player1_turn == 4 and mid_left == None:
-    mid_left = "X"
-if player1_turn == 5 and mid_mid == None:
-    mid_mid = "X"
-if player1_turn == 6 and mid_right == None:
-    mid_right = "X"
-if player1_turn == 7 and bottom_left == None:
-    bottom_left = "X"
-if player1_turn == 8 and bottom_mid == None:
-    bottom_mid = "X"
-if player1_turn == 9 and bottom_right == None:
-    bottom_right = "X"
+    # PLAYER TWO INPUT
+    while True:
+            try:
+                player2_turn = int(input("Choose a spot based on the correlating number from the diagram above: "))
+            except:
+                print("THAT'S NOT A NUMBER!!!")
+            else:
+                if player2_turn < 0 or player2_turn > 9:
+                    print("THAT'S NOT AN OPTION!!!")
+                else:
+                    break
+
+
+    # PLAYER TWO OUTPUT
+    if player2_turn == 1 and top_left == " ":
+        top_left = "O"
+    elif player2_turn == 2 and top_mid == " ":
+        top_mid = "O"
+    elif player2_turn == 3 and top_right == " ":
+        top_right = "O"
+    elif player2_turn == 4 and mid_left == " ":
+        mid_left = "O"
+    elif player2_turn == 5 and mid_mid == " ":
+        mid_mid = "O"
+    elif player2_turn == 6 and mid_right == " ":
+        mid_right = "O"
+    elif player2_turn == 7 and bottom_left == " ":
+        bottom_left = "O"
+    elif player2_turn == 8 and bottom_mid == " ":
+        bottom_mid = "O"
+    elif player2_turn == 9 and bottom_right == " ":
+        bottom_right = "O"
+    else:
+        print("YOU FORFEIT YOUR TURN FOR CHOOSING A SPACE THAT WAS TAKEN UP!")
+
+
+    gameBoard()
