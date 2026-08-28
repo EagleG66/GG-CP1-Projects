b2b3a0a1d175e048432143359f87636c5244d7e2
EagleG66 <glenn.gudmunson@ucas-edu.net>
2026-08-28 09:53:43 -0600

    Finished TIC TAC TOE!!!

--- Im_bored.py
+++ Im_bored.py
@@ -165,7 +165,7 @@ print("   7   |   8   |   9   ")
 print("       |       |       ")
 
 
-
+# GAMEBOARD
 def gameBoard():
     print("       |       |       ")
     print("   "+top_left+"   |   "+top_mid+"   |   "+top_right+"   ")
@@ -180,13 +180,81 @@ def gameBoard():
     print("       |       |       ")
 
 
+def winCheck(shape):
+
+    global game_running
+
+    if top_left == shape and top_mid == shape and top_right == shape:
+        if shape == "X":
+            print("Player 1 Wins!!!")
+            game_running = False
+        elif shape == "O":
+            print("Player 2 Wins!!!")
+            game_running = False
+    elif mid_left == shape and mid_mid == shape and mid_right == shape:
+        if shape == "X":
+            print("Player 1 Wins!!!")
+            game_running = False
+        elif shape == "O":
+            print("Player 2 Wins!!!")
+            game_running = False
+    elif bottom_left == shape and bottom_mid == shape and bottom_right == shape:
+        if shape == "X":
+            print("Player 1 Wins!!!")
+            game_running = False
+        elif shape == "O":
+            print("Player 2 Wins!!!")
+            game_running = False
+    elif top_left == shape and mid_left == shape and bottom_left == shape:
+        if shape == "X":
+            print("Player 1 Wins!!!")
+            game_running = False
+        elif shape == "O":
+            print("Player 2 Wins!!!")
+            game_running = False
+    elif top_mid == shape and mid_mid == shape and bottom_mid == shape:
+        if shape == "X":
+            print("Player 1 Wins!!!")
+            game_running = False
+        elif shape == "O":
+            print("Player 2 Wins!!!")
+            game_running = False
+    elif top_right == shape and mid_right == shape and bottom_right == shape:
+        if shape == "X":
+            print("Player 1 Wins!!!")
+            game_running = False
+        elif shape == "O":
+            print("Player 2 Wins!!!")
+            game_running = False
+    elif top_left == shape and mid_mid == shape and bottom_right == shape:
+        if shape == "X":
+            print("Player 1 Wins!!!")
+            game_running = False
+        elif shape == "O":
+            print("Player 2 Wins!!!")
+            game_running = False
+    elif top_right == shape and mid_mid == shape and bottom_left == shape:
+        if shape == "X":
+            print("Player 1 Wins!!!")
+            game_running = False
+        elif shape == "O":
+            print("Player 2 Wins!!!")
+            game_running = False
+
+
+
 
 while game_running:
+
+    # CHECKING FOR A WIN
+    winCheck("O")
+    winCheck("X")
+
 
     # PLAYER ONE INPUT
     while True:
         try:
-            player1_turn = int(input("Choose a spot based on the correlating number from the diagram above: "))
+            player1_turn = int(input("Player1: Choose a spot based on the correlating number from the diagram above: "))
         except:
             print("THAT'S NOT A NUMBER!!!")
         else:
@@ -218,13 +286,19 @@ while game_running:
     else:
         print("YOU FORFEIT YOUR TURN FOR CHOOSING A SPACE THAT WAS TAKEN UP!")
 
+    # PRINT THE BOARD
     gameBoard()
 
 
-    # PLAYER TWO INPUT
-    while True:
+    # CHECKING FOR A WIN
+    winCheck("O")
+    winCheck("X")
+
+    if game_running:
+        # PLAYER TWO INPUT
+        while True:
             try:
-                player2_turn = int(input("Choose a spot based on the correlating number from the diagram above: "))
+                player2_turn = int(input("Player2: Choose a spot based on the correlating number from the diagram above: "))
             except:
                 print("THAT'S NOT A NUMBER!!!")
             else:
@@ -234,27 +308,27 @@ while game_running:
                     break
 
 
-    # PLAYER TWO OUTPUT
-    if player2_turn == 1 and top_left == " ":
-        top_left = "O"
-    elif player2_turn == 2 and top_mid == " ":
-        top_mid = "O"
-    elif player2_turn == 3 and top_right == " ":
-        top_right = "O"
-    elif player2_turn == 4 and mid_left == " ":
-        mid_left = "O"
-    elif player2_turn == 5 and mid_mid == " ":
-        mid_mid = "O"
-    elif player2_turn == 6 and mid_right == " ":
-        mid_right = "O"
-    elif player2_turn == 7 and bottom_left == " ":
-        bottom_left = "O"
-    elif player2_turn == 8 and bottom_mid == " ":
-        bottom_mid = "O"
-    elif player2_turn == 9 and bottom_right == " ":
-        bottom_right = "O"
-    else:
-        print("YOU FORFEIT YOUR TURN FOR CHOOSING A SPACE THAT WAS TAKEN UP!")
+        # PLAYER TWO OUTPUT
+        if player2_turn == 1 and top_left == " ":
+            top_left = "O"
+        elif player2_turn == 2 and top_mid == " ":
+            top_mid = "O"
+        elif player2_turn == 3 and top_right == " ":
+            top_right = "O"
+        elif player2_turn == 4 and mid_left == " ":
+            mid_left = "O"
+        elif player2_turn == 5 and mid_mid == " ":
+            mid_mid = "O"
+        elif player2_turn == 6 and mid_right == " ":
+            mid_right = "O"
+        elif player2_turn == 7 and bottom_left == " ":
+            bottom_left = "O"
+        elif player2_turn == 8 and bottom_mid == " ":
+            bottom_mid = "O"
+        elif player2_turn == 9 and bottom_right == " ":
+            bottom_right = "O"
+        else:
+            print("YOU FORFEIT YOUR TURN FOR CHOOSING A SPACE THAT WAS TAKEN UP!")
 
-
-    gameBoard()
+        # DRAW THE BOARD
+        gameBoard()
