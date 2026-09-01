49b199c442a8c430a9627ffa9561de1146e72149
EagleG66 <glenn.gudmunson@ucas-edu.net>
2026-09-01 09:45:57 -0600

    added a try/except to avg grade

--- Assignments/average_grade.py
+++ Assignments/average_grade.py
@@ -1,12 +1,63 @@
 # GLENN GUDMUNSON Average Grade
 
-class1 = int(input("What percent do you have in your first period class? "))
-class2 = int(input("What percent do you have in your second period class? "))
-class3 = int(input("What percent do you have in your third period class? "))
-class4 = int(input("What percent do you have in your advisory class? "))
-class5 = int(input("What percent do you have in your sixth period class? "))
-class6 = int(input("What percent do you have in your seventh period class? "))
-class7 = int(input("What percent do you have in your eighth period class? "))
+while True:
+    try:
+        class1 = int(input("What percent do you have in your first period class? "))
+    except:
+        print("THAT'S NOT A NUMBER!!!")
+    else:
+        break
+
+while True:
+    try:
+        class2 = int(input("What percent do you have in your second period class? "))
+    except:
+        print("THAT'S NOT A NUMBER!!!")
+    else:
+        break
+
+while True:
+    try:
+        class3 = int(input("What percent do you have in your third period class? "))
+    except:
+        print("THAT'S NOT A NUMBER!!!")
+    else:
+        break
+
+while True:
+    try:
+        class4 = int(input("What percent do you have in your advisory class? "))
+    except:
+        print("THAT'S NOT A NUMBER!!!")
+    else:
+        break
+
+while True:
+    try:
+        class5 = int(input("What percent do you have in your sixth period class? "))
+    except:
+        print("THAT'S NOT A NUMBER!!!")
+    else:
+        break
+
+while True:
+    try:
+        class6 = int(input("What percent do you have in your seventh period class? "))
+    except:
+        print("THAT'S NOT A NUMBER!!!")
+    else:
+        break
+
+while True:
+    try:
+        class7 = int(input("What percent do you have in your eighth period class? "))
+    except:
+        print("THAT'S NOT A NUMBER!!!")
+    else:
+        break
+
+
+
 
 avg_grade = (class1+class2+class3+class4+class5+class6+class7)/7
 
