eb82194224b6f9c2f5e65afd658d9d9cc5708829
EagleG66 <glenn.gudmunson@ucas-edu.net>
2026-09-01 09:36:02 -0600

    fixed avg grade

--- Assignments/average_grade.py
+++ Assignments/average_grade.py
@@ -1,12 +1,12 @@
 # GLENN GUDMUNSON Average Grade
 
-class1 = input("What percent do you have in your first period class? ")
-class2 = input("What percent do you have in your second period class? ")
-class3 = input("What percent do you have in your third period class? ")
-class4 = input("What percent do you have in your advisory class? ")
-class5 = input("What percent do you have in your sixth period class? ")
-class6 = input("What percent do you have in your seventh period class? ")
-class7 = input("What percent do you have in your eighth period class? ")
+class1 = int(input("What percent do you have in your first period class? "))
+class2 = int(input("What percent do you have in your second period class? "))
+class3 = int(input("What percent do you have in your third period class? "))
+class4 = int(input("What percent do you have in your advisory class? "))
+class5 = int(input("What percent do you have in your sixth period class? "))
+class6 = int(input("What percent do you have in your seventh period class? "))
+class7 = int(input("What percent do you have in your eighth period class? "))
 
 avg_grade = (class1+class2+class3+class4+class5+class6+class7)/7
 
