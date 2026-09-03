ac9b2c74909f00f554d66a6c09ba460e3aac3667
EagleG66 <glenn.gudmunson@ucas-edu.net>
2026-09-03 09:53:23 -0600

    made some game files and started them

---
+++ I'm bored/BATTLESHIP.py
@@ -0,0 +1,4 @@
+
+
+def gameBoard():
+    print("     |     |     |     |     |     |     |     |     |     |     ")
--- I'm bored/Black_Jack.py
+++ I'm bored/Black_Jack.py
@@ -1,3 +1,7 @@
 import random
 
-p1_card1 = random.randint(0,1)
+p1_card1 = random.randint(0,14)
+p1_card2 = random.randint(0,14)
+
+p2_card1 = random.randint(0,14)
+p2_card2 = random.randint(0,14)
