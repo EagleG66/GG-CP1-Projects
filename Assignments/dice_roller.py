96cab55d7c4a48aec6f917498c8aae242dfa9192
EagleG66 <glenn.gudmunson@ucas-edu.net>
2026-09-10 09:21:57 -0600

    Made dice roller and worked on BATLLEFRICKIN SHIP

---
+++ Assignments/dice_roller.py
@@ -0,0 +1,30 @@
+# GLENN GUDMUNSON dice roller
+
+import random
+
+while True:
+    try:
+        dice_type = input("Out of these dice, which one do you want to roll?(D4, D6, D8, D10, D12, D20): ").capitalize()
+    except:
+        print("That's not right!!!")
+    else:
+        if dice_type != "D4" and dice_type != "D6" and dice_type != "D8" and dice_type != "D10" and dice_type != "D12" and dice_type != "D20":
+            print("THAT'S NOT AN OPTION!!!")
+        else:
+            break
+
+
+if dice_type == "D4":
+    dice_roll = random.randint(1,5)
+elif dice_type == "D6":
+    dice_roll = random.randint(1,7)
+elif dice_type == "D8":
+    dice_roll = random.randint(1,9)
+elif dice_type == "D10":
+    dice_roll = random.randint(1,11)
+elif dice_type == "D12":
+    dice_roll = random.randint(1,13)
+elif dice_type == "D20":
+    dice_roll = random.randint(1,21)
+
+print(f"You rolled a {dice_roll}")
