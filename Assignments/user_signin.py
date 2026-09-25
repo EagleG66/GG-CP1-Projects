bb362b97dea4347b361d2fb3d8961852313e599d
EagleG66 <glenn.gudmunson@ucas-edu.net>
2026-09-25 08:59:53 -0600

    user sign in

--- Assignments/user_signin.py
+++ Assignments/user_signin.py
@@ -5,8 +5,8 @@ password = "123456789"
 
 while True:
         
-    username_input = input("What is your username:")
-    password_input = input("What is your password:")
+    username_input = input("What is your username: ")
+    password_input = input("What is your password: ")
 
     if username == username_input and password == password_input:
         print("You have officially signed in!!!")
